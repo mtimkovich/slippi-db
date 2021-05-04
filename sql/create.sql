5227ee94ae5977f1cc288ee7dccf25b2e0868303
Max Timkovich <max@timkovi.ch>
2021-05-03 22:10:42 -0600

    fix sql

--- sql/create.sql
+++ sql/create.sql
@@ -1,17 +1,23 @@
 create table games (
     id integer primary key not null,
-    foreign key(filename) references slps(filename),
+    filename text not null,
     is_teams boolean not null,
     start_time datetime not null,
     stage text not null,
     duration real not null,
-    foreign key(port1) references players(id),
-    foreign key(port2) references players(id),
-    foreign key(port3) references players(id),
-    foreign key(port4) references players(id),
+    port1 integer,
+    port2 integer,
+    port3 integer,
+    port4 integer,
     -- json
     teams text,
     winners text not null,
+
+    foreign key(filename) references slps(filename),
+    foreign key(port1) references players(id),
+    foreign key(port2) references players(id),
+    foreign key(port3) references players(id),
+    foreign key(port4) references players(id)
 );
 
 create table players (
@@ -19,9 +25,9 @@ create table players (
     tag text not null,
     code text not null,
     -- `character` is a reserved word lol
-    fighter text not null,
+    fighter text not null
 );
 
 create table slps (
-    filename text primary key not null,
+    filename text primary key not null
 );
