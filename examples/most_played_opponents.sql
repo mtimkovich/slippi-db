1ab0c55e0f4c91f09aa2c861798118c8726ea5ab
Max Timkovich <max@timkovi.ch>
2021-05-16 10:52:18 -0600

    add most played opponent

---
+++ examples/most_played_opponents.sql
@@ -0,0 +1,16 @@
+-- Get the people you've played the most amount of games with.
+-- Replace 'DJSwerve' with your tag unless your tag is DJSwerve.
+
+with my_games as (
+    select op.tag as op
+    from players me, players op
+    where op.id != me.id
+    and me.game_id = op.game_id
+    and me.tag = 'DJSwerve')
+
+select
+    op,
+    count(op) as cnt
+from my_games
+group by op
+order by cnt desc;
