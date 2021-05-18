f931110dfe631a44d04cc434db72d1463f47bb8b
Max Timkovich <max@timkovi.ch>
2021-05-18 10:26:36 -0600

    more examples

--- examples/win_rate_against_falco_on_fd.sql
+++ examples/win_rate_against_falco_on_fd.sql
@@ -1,12 +1,15 @@
--- Calculate all time win rate in singles.
+-- Win rate against Falco on FD (a personal favorite).
 -- Replace 'DJSwerve' with your tag unless your tag is DJSwerve.
 
 with my_games as (
     select me.*
-    from players me, players op
+    from players me, players op, games
     where op.id != me.id
     and me.game_id = op.game_id
+    and me.game_id = games.id
     and me.team is null
+    and op.character = 'FALCO'
+    and games.stage = 'FINAL_DESTINATION'
     and me.tag = 'DJSwerve')
 
 select avg(winner) * 100 from my_games;
--- examples/win_rate_last_week.sql
+++ examples/win_rate_last_week.sql
@@ -1,12 +1,14 @@
--- Calculate all time win rate in singles.
+-- Calculate singles win rate over the past week.
 -- Replace 'DJSwerve' with your tag unless your tag is DJSwerve.
 
 with my_games as (
     select me.*
-    from players me, players op
+    from players me, players op, games
     where op.id != me.id
     and me.game_id = op.game_id
-    and me.team is null
+    and games.id = me.game_id
+    and not games.is_teams
+    and julianday() - julianday(games.start_time) < 7
     and me.tag = 'DJSwerve')
 
 select avg(winner) * 100 from my_games;
